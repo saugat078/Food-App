845c97e6c56f459c5fc997b011a4caadab2ac9fc
saugat078 <saugat.poudel478@gmail.com>
2024-08-15 15:10:31 +0545

    Added swiper for offerImages from Backend

--- grocery_shop_app/lib/screens/home_screen.dart
+++ grocery_shop_app/lib/screens/home_screen.dart
@@ -1,3 +1,176 @@
+// import 'package:card_swiper/card_swiper.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_iconly/flutter_iconly.dart';
+// import 'package:grocery_shop_app/consts/constss.dart';
+// import 'package:grocery_shop_app/inner_screens/feeds_screen.dart';
+// import 'package:grocery_shop_app/inner_screens/on_sale_screen.dart';
+// import 'package:grocery_shop_app/models/products_model.dart';
+// import 'package:grocery_shop_app/providers/products_provider.dart';
+// import 'package:grocery_shop_app/services/global_methods.dart';
+// import 'package:grocery_shop_app/services/utils.dart';
+// import 'package:grocery_shop_app/widgets/feed_items.dart';
+// import 'package:grocery_shop_app/widgets/text_widget.dart';
+// import 'package:provider/provider.dart';
+
+// import '../widgets/on_sale_widget.dart';
+
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({Key? key}) : super(key: key);
+
+//   @override
+//   State<HomeScreen> createState() => _HomeScreenState();
+// }
+
+// class _HomeScreenState extends State<HomeScreen> {
+//   final List<String> _offerImages = [
+//     'assets/images/offres/Offer1.jpg',
+//     'assets/images/offres/Offer2.jpg',
+//     'assets/images/offres/Offer3.jpg',
+//     'assets/images/offres/Offer4.jpg'
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final Utils utils = Utils(context);
+//     final themeState = utils.getTheme;
+//     Size size = utils.getScreenSize;
+//     final Color color = Utils(context).color;
+//     final productProviders = Provider.of<ProductsProvider>(context);
+//     List<ProductModel> allProducts = productProviders.getProducts;
+//         List<ProductModel> productsOnSale = productProviders.getOnSaleProducts;
+//     return Scaffold(
+//       body: SingleChildScrollView(
+//         child: Column(
+//           children: [
+//             SizedBox(
+//               height: size.height * 0.33,
+//               child: Swiper(
+//                 itemBuilder: (BuildContext context, int index) {
+//                   return Image.asset(
+//                     _offerImages[index],
+//                     fit: BoxFit.fill,
+//                   );
+//                 },
+//                 autoplay: true,
+//                 itemCount: _offerImages.length,
+//                 pagination: const SwiperPagination(
+//                     alignment: Alignment.bottomCenter,
+//                     builder: DotSwiperPaginationBuilder(
+//                         color: Colors.white, activeColor: Colors.red)),
+//                 // control: const SwiperControl(color: Colors.black),
+//               ),
+//             ),
+//             const SizedBox(
+//               height: 6,
+//             ),
+//             TextButton(
+//               onPressed: () {
+//                 GlobalMethods.navigateTo(
+//                     ctx: context, routeName: OnSaleScreen.routeName);
+//               },
+//               child: TextWidget(
+//                 text: 'View all',
+//                 maxLines: 1,
+//                 color: Colors.blue,
+//                 textSize: 20,
+//               ),
+//             ),
+//             const SizedBox(
+//               height: 6,
+//             ),
+//             Row(
+//               children: [
+//                 RotatedBox(
+//                   quarterTurns: -1,
+//                   child: Row(
+//                     children: [
+//                       TextWidget(
+//                         text: 'On sale'.toUpperCase(),
+//                         color: Colors.red,
+//                         textSize: 22,
+//                         isTitle: true,
+//                       ),
+//                       const SizedBox(
+//                         width: 5,
+//                       ),
+//                       const Icon(
+//                         IconlyLight.discount,
+//                         color: Colors.red,
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//                 const SizedBox(
+//                   width: 8,
+//                 ),
+//                 Flexible(
+//                   child: SizedBox(
+//                     height: size.height * 0.18,
+//                     child: ListView.builder(
+//                         itemCount: productsOnSale.length < 10 ? productsOnSale.length : 10,
+//                         scrollDirection: Axis.horizontal,
+//                         itemBuilder: (ctx, index) {
+//                           return Padding(
+//                             padding: const EdgeInsets.symmetric(horizontal: 8),
+//                             child: ChangeNotifierProvider.value(
+//                                               value: productsOnSale[index], child: const OnSaleWidget()),
+//                           );
+//                         }),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             const SizedBox(
+//               height: 10,
+//             ),
+//             Padding(
+//               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   TextWidget(
+//                     text: 'Our products',
+//                     color: color,
+//                     textSize: 22,
+//                     isTitle: true,
+//                   ),
+//                   // const Spacer(),
+//                   TextButton(
+//                     onPressed: () {
+//                       GlobalMethods.navigateTo(
+//                           ctx: context, routeName: FeedsScreen.routeName);
+//                     },
+//                     child: TextWidget(
+//                       text: 'Browse all',
+//                       maxLines: 1,
+//                       color: Colors.blue,
+//                       textSize: 20,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             GridView.count(
+//               shrinkWrap: true,
+//               physics: const NeverScrollableScrollPhysics(),
+//               crossAxisCount: 2,
+//               crossAxisSpacing: 20,
+//               mainAxisSpacing: 20,
+//               padding: const EdgeInsets.symmetric(horizontal: 15),
+//               childAspectRatio: size.width / (size.height * 0.55),
+//               children: List.generate(
+//                   allProducts.length < 4 ? allProducts.length : 4, (index) {
+//                 return ChangeNotifierProvider.value(
+//                     value: allProducts[index], child: const FeedsWidget());
+//               }),
+//             ),
+//             const SizedBox(height: 20,),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
@@ -11,6 +184,7 @@ import 'package:grocery_shop_app/services/utils.dart';
 import 'package:grocery_shop_app/widgets/feed_items.dart';
 import 'package:grocery_shop_app/widgets/text_widget.dart';
 import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../widgets/on_sale_widget.dart';
 
@@ -22,12 +196,26 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<String> _offerImages = [
-    'assets/images/offres/Offer1.jpg',
-    'assets/images/offres/Offer2.jpg',
-    'assets/images/offres/Offer3.jpg',
-    'assets/images/offres/Offer4.jpg'
-  ];
+  List<String> _offerImages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchOfferImages();
+  }
+
+  Future<void> _fetchOfferImages() async {
+    try {
+      final QuerySnapshot snapshot = await FirebaseFirestore.instance.collection('offers').get();
+      final List<String> imageUrls = snapshot.docs.map((doc) => doc['imageUrl'] as String).toList();
+
+      setState(() {
+        _offerImages = imageUrls;
+      });
+    } catch (e) {
+      print('Error fetching images: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +225,8 @@ class _HomeScreenState extends State<HomeScreen> {
     final Color color = Utils(context).color;
     final productProviders = Provider.of<ProductsProvider>(context);
     List<ProductModel> allProducts = productProviders.getProducts;
-        List<ProductModel> productsOnSale = productProviders.getOnSaleProducts;
+    List<ProductModel> productsOnSale = productProviders.getOnSaleProducts;
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -46,23 +235,22 @@ class _HomeScreenState extends State<HomeScreen> {
               height: size.height * 0.33,
               child: Swiper(
                 itemBuilder: (BuildContext context, int index) {
-                  return Image.asset(
-                    _offerImages[index],
-                    fit: BoxFit.fill,
-                  );
+                  return _offerImages.isEmpty
+                      ? Center(child: CircularProgressIndicator())
+                      : Image.network(
+                          _offerImages[index],
+                          fit: BoxFit.fill,
+                        );
                 },
                 autoplay: true,
-                itemCount: _offerImages.length,
+                itemCount: _offerImages.isEmpty ? 1 : _offerImages.length,
                 pagination: const SwiperPagination(
                     alignment: Alignment.bottomCenter,
                     builder: DotSwiperPaginationBuilder(
                         color: Colors.white, activeColor: Colors.red)),
-                // control: const SwiperControl(color: Colors.black),
               ),
             ),
-            const SizedBox(
-              height: 6,
-            ),
+            const SizedBox(height: 6),
             TextButton(
               onPressed: () {
                 GlobalMethods.navigateTo(
@@ -75,9 +263,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 textSize: 20,
               ),
             ),
-            const SizedBox(
-              height: 6,
-            ),
+            const SizedBox(height: 6),
             Row(
               children: [
                 RotatedBox(
@@ -90,19 +276,12 @@ class _HomeScreenState extends State<HomeScreen> {
                         textSize: 22,
                         isTitle: true,
                       ),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      const Icon(
-                        IconlyLight.discount,
-                        color: Colors.red,
-                      ),
+                      const SizedBox(width: 5),
+                      const Icon(IconlyLight.discount, color: Colors.red),
                     ],
                   ),
                 ),
-                const SizedBox(
-                  width: 8,
-                ),
+                const SizedBox(width: 8),
                 Flexible(
                   child: SizedBox(
                     height: size.height * 0.18,
@@ -113,16 +292,15 @@ class _HomeScreenState extends State<HomeScreen> {
                           return Padding(
                             padding: const EdgeInsets.symmetric(horizontal: 8),
                             child: ChangeNotifierProvider.value(
-                                              value: productsOnSale[index], child: const OnSaleWidget()),
+                                value: productsOnSale[index],
+                                child: const OnSaleWidget()),
                           );
                         }),
                   ),
                 ),
               ],
             ),
-            const SizedBox(
-              height: 10,
-            ),
+            const SizedBox(height: 10),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
               child: Row(
@@ -134,7 +312,6 @@ class _HomeScreenState extends State<HomeScreen> {
                     textSize: 22,
                     isTitle: true,
                   ),
-                  // const Spacer(),
                   TextButton(
                     onPressed: () {
                       GlobalMethods.navigateTo(
@@ -164,7 +341,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     value: allProducts[index], child: const FeedsWidget());
               }),
             ),
-            const SizedBox(height: 20,),
+            const SizedBox(height: 20),
           ],
         ),
       ),
